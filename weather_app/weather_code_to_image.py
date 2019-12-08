1c02ba6403438ff53993c4a3c055a6e6c294903a
JamezGr <james.p.grimes1999@gmail.com>
2019-12-08 17:39:45 +0000

    Five Day Weather Icons Added based on Weather Code

--- weather_app/weather_code_to_image.py
+++ weather_app/weather_code_to_image.py
@@ -1,148 +1,148 @@
 def condition(weather_code):
     if weather_code == '395':
-        return ['Cloud-Snow-Alt.svg', 'Cloud-Snow-Moon-Alt.svg', 'Moderate or heavy snow in area with thunder']
+        return ['img/Weather_Icons/Cloud-Snow-Alt.svg', 'Cloud-Snow-Moon-Alt.svg', 'Moderate or heavy snow in area with thunder']
 
     if weather_code == '392':
-        return ['Cloud-Snow-Alt.svg', 'Cloud-Snow-Moon-Alt.svg', 'Patchy light snow in area with thunder']
+        return ['img/Weather_Icons/Cloud-Snow-Alt.svg', 'Cloud-Snow-Moon-Alt.svg', 'Patchy light snow in area with thunder']
 
     if weather_code == '389':
-        return ['Cloud-Lightning.svg', 'Cloud-Lightning-Moon.svg', 'Moderate or heavy rain in area with thunder']
+        return ['img/Weather_Icons/Cloud-Lightning.svg', 'Cloud-Lightning-Moon.svg', 'Moderate or heavy rain in area with thunder']
 
     if weather_code == '386':
-        return ['Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Patchy light rain in area with thunder']
+        return ['img/Weather_Icons/Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Patchy light rain in area with thunder']
 
     if weather_code == '386':
-        return ['Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Patchy light rain in area with thunder']
+        return ['img/Weather_Icons/Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Patchy light rain in area with thunder']
 
     if weather_code == '377':
-        return ['Cloud-Hail-Alt.svg', 'Cloud-Hail-Moon-Alt.svg', 'Moderate or heavy showers of ice pellets']
+        return ['img/Weather_Icons/Cloud-Hail-Alt.svg', 'Cloud-Hail-Moon-Alt.svg', 'Moderate or heavy showers of ice pellets']
 
     if weather_code == '374':
-        return ['Cloud-Hail.svg', 'Cloud-Hail-Moon.svg', 'Light showers of ice pellets']
+        return ['img/Weather_Icons/Cloud-Hail.svg', 'Cloud-Hail-Moon.svg', 'Light showers of ice pellets']
 
     if weather_code == '371':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Moderate or heavy snow showers']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Moderate or heavy snow showers']
 
     if weather_code == '368':
-        return ['Cloud-Snow-Alt.svg', 'Cloud-Snow-Moon-Alt.svg', 'Light snow showers']
+        return ['img/Weather_Icons/Cloud-Snow-Alt.svg', 'Cloud-Snow-Moon-Alt.svg', 'Light snow showers']
 
     if weather_code == '365':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Moderate or heavy sleet showers']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Moderate or heavy sleet showers']
 
     if weather_code == '362':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Light sleet showers']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Light sleet showers']
 
     if weather_code == '359':
-        return ['Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Torrential rain shower']
+        return ['img/Weather_Icons/Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Torrential rain shower']
 
     if weather_code == '356':
-        return ['Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Moderate or heavy rain shower']
+        return ['img/Weather_Icons/Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Moderate or heavy rain shower']
 
     if weather_code == '353':
-        return ['Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Light rain shower']
+        return ['img/Weather_Icons/Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Light rain shower']
 
     if weather_code == '350':
-        return ['Cloud-Hail-Alt.svg', 'Cloud-Hail-Moon-Alt.svg', 'Ice pellets']
+        return ['img/Weather_Icons/Cloud-Hail-Alt.svg', 'Cloud-Hail-Moon-Alt.svg', 'Ice pellets']
 
     if weather_code == '338':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Heavy snow']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Heavy snow']
 
     if weather_code == '335':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Patchy heavy snow']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Patchy heavy snow']
 
     if weather_code == '332':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Moderate snow']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Moderate snow']
 
     if weather_code == '329':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Patchy moderate snow']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Patchy moderate snow']
 
     if weather_code == '326':
-        return ['Cloud-Snow-Alt.svg', 'Cloud-Snow-Moon-Alt.svg', 'Light snow']
+        return ['img/Weather_Icons/Cloud-Snow-Alt.svg', 'Cloud-Snow-Moon-Alt.svg', 'Light snow']
 
     if weather_code == '323':
-        return ['Cloud-Snow-Alt.svg', 'Cloud-Snow-Moon-Alt.svg', 'Patchy light snow']
+        return ['img/Weather_Icons/Cloud-Snow-Alt.svg', 'Cloud-Snow-Moon-Alt.svg', 'Patchy light snow']
 
     if weather_code == '320':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Moderate or heavy sleet']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Moderate or heavy sleet']
 
     if weather_code == '317':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Light sleet']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Light sleet']
 
     if weather_code == '314':
-        return ['Cloud-Drizzle.svg', 'Cloud-Drizzle-Moon.svg', 'Moderate or Heavy freezing rain']
+        return ['img/Weather_Icons/Cloud-Drizzle.svg', 'Cloud-Drizzle-Moon.svg', 'Moderate or Heavy freezing rain']
 
     if weather_code == '311':
-        return ['Cloud-Drizzle-Alt.svg', 'Cloud-Drizzle-Moon-Alt.svg', 'Light freezing rain']
+        return ['img/Weather_Icons/Cloud-Drizzle-Alt.svg', 'Cloud-Drizzle-Moon-Alt.svg', 'Light freezing rain']
 
     if weather_code == '308':
-        return ['Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Heavy rain']
+        return ['img/Weather_Icons/Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Heavy rain']
 
     if weather_code == '305':
-        return ['Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Heavy rain at times']
+        return ['img/Weather_Icons/Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Heavy rain at times']
 
     if weather_code == '302':
-        return ['Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Moderate rain']
+        return ['img/Weather_Icons/Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Moderate rain']
 
     if weather_code == '299':
-        return ['Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Moderate rain at times']
+        return ['img/Weather_Icons/Cloud-Rain.svg', 'Cloud-Rain-Moon.svg', 'Moderate rain at times']
 
     if weather_code == '296':
-        return ['Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Light rain']
+        return ['img/Weather_Icons/Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Light rain']
 
     if weather_code == '293':
-        return ['Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Patchy light rain']
+        return ['img/Weather_Icons/img/Weather_Icons/Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Patchy light rain']
 
     if weather_code == '284':
-        return ['Cloud-Drizzle.svg', 'Cloud-Drizzle-Moon.svg', 'Heavy freezing drizzle']
+        return ['img/Weather_Icons/img/Weather_Icons/Cloud-Drizzle.svg', 'Cloud-Drizzle-Moon.svg', 'Heavy freezing drizzle']
 
     if weather_code == '281':
-        return ['Cloud-Drizzle.svg', 'Cloud-Drizzle-Moon.svg', 'Freezing drizzle']
+        return ['img/Weather_Icons/img/Weather_Icons/Cloud-Drizzle.svg', 'Cloud-Drizzle-Moon.svg', 'Freezing drizzle']
 
     if weather_code == '266':
-        return ['Cloud-Drizzle-Alt.svg', 'Cloud-Drizzle-Moon-Alt.svg', 'Light drizzle']
+        return ['img/Weather_Icons/img/Weather_Icons/Cloud-Drizzle-Alt.svg', 'Cloud-Drizzle-Moon-Alt.svg', 'Light drizzle']
 
     if weather_code == '263':
-        return ['Cloud-Drizzle-Alt.svg', 'Cloud-Drizzle-Moon-Alt.svg', 'Patchy light drizzle']
+        return ['img/Weather_Icons/img/Weather_Icons/Cloud-Drizzle-Alt.svg', 'Cloud-Drizzle-Moon-Alt.svg', 'Patchy light drizzle']
 
     if weather_code == '260':
-        return ['Cloud-Fog-Alt.svg', 'Cloud-Fog-Moon-Alt.svg', 'Freezing fog']
+        return ['img/Weather_Icons/Cloud-Fog-Alt.svg', 'Cloud-Fog-Moon-Alt.svg', 'Freezing fog']
 
     if weather_code == '248':
-        return ['Cloud-Fog.svg', 'Cloud-Fog-Moon.svg', 'Fog']
+        return ['img/Weather_Icons/Cloud-Fog.svg', 'Cloud-Fog-Moon.svg', 'Fog']
 
     if weather_code == '230':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Blizzard']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Blizzard']
 
     if weather_code == '227':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Blowing snow']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Blowing snow']
 
     if weather_code == '200':
-        return ['Cloud-Lightning.svg', 'Cloud-Lightning-Moon.svg', 'Thundery outbreaks in nearby']
+        return ['img/Weather_Icons/Cloud-Lightning.svg', 'Cloud-Lightning-Moon.svg', 'Thundery outbreaks in nearby']
 
     if weather_code == '185':
-        return ['Cloud-Drizzle.svg', 'Cloud-Drizzle-Moon.svg', 'Patchy freezing drizzle nearby']
+        return ['img/Weather_Icons/Cloud-Drizzle.svg', 'Cloud-Drizzle-Moon.svg', 'Patchy freezing drizzle nearby']
 
     if weather_code == '182':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Patchy sleet nearby']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Patchy sleet nearby']
 
     if weather_code == '179':
-        return ['Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Patchy snow nearby']
+        return ['img/Weather_Icons/Cloud-Snow.svg', 'Cloud-Snow-Moon.svg', 'Patchy snow nearby']
 
     if weather_code == '176':
-        return ['Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Patchy rain nearby']
+        return ['img/Weather_Icons/Cloud-Rain-Alt.svg', 'Cloud-Rain-Moon-Alt.svg', 'Patchy rain nearby']
 
     if weather_code == '143':
-        return ['Cloud-Fog-Alt.svg', 'Cloud-Fog-Moon-Alt.svg', 'Mist']
+        return ['img/Weather_Icons/Cloud-Fog-Alt.svg', 'Cloud-Fog-Moon-Alt.svg', 'Mist']
 
     if weather_code == '122':
-        return ['Cloud.svg', 'Cloud-Moon.svg', 'Overcast']
+        return ['img/Weather_Icons/Cloud.svg', 'Cloud-Moon.svg', 'Overcast']
 
     if weather_code == '119':
-        return ['Cloud.svg', 'Cloud-Moon.svg', 'Cloudy']
+        return ['img/Weather_Icons/Cloud.svg', 'Cloud-Moon.svg', 'Cloudy']
 
     if weather_code == '116':
-        return ['Cloud.svg', 'Cloud-Moon.svg', 'Partly Cloudy']
+        return ['img/Weather_Icons/Cloud.svg', 'Cloud-Moon.svg', 'Partly Cloudy']
 
     if weather_code == '113':
-        return ['Sun.svg', 'Moon.svg', 'Clear/Sunny']
+        return ['img/Weather_Icons/Sun.svg', 'Moon.svg', 'Clear/Sunny']
 
